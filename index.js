53cd601d0efc914581824ecef9d33aaf4d8124ec
ciao338 <ginopaltrendara@gmail.com>
2026-07-01 00:19:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 
-// Inserisci qui i tuoi dati
-const TOKEN = 'MTUyMTYzNzQzMDE4MzA2NzcwOQ.GVn9es.WCCPqZX76_Jw_pgJ-fkIsprYWmktr6V7YFBbsM';
-const CLIENT_ID = '1521637430183067709';
-
 const client = new Client({ 
     intents: [
         GatewayIntentBits.Guilds, 
@@ -13,8 +9,7 @@ const client = new Client({
 });
 
 client.once('ready', () => {
-    console.log(`Bot online! Loggato come ${client.user.tag}`);
-    console.log(`ID Cliente: ${CLIENT_ID}`);
+    console.log(`Bot online come ${client.user.tag}!`);
 });
 
 client.on('messageCreate', (message) => {
@@ -23,4 +18,5 @@ client.on('messageCreate', (message) => {
     }
 });
 
-client.login(TOKEN);
+// Questa riga legge il token direttamente da Railway, senza mostrarlo nel codice
+client.login(process.env.DISCORD_TOKEN);
